abc0cad48a37b1fd04a62adaefcc6e84cc533a5d
Achille <125024135+LostBoy404@users.noreply.github.com>
2023-07-20 23:49:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,44 +49,6 @@ function createFlexItems(container, count) {
   }
 }
 
-// Function to set the image as a background of the flex item
-function setImageAsBackground(flexItem, imageUrl) {
-  flexItem.style.backgroundImage = `url('${imageUrl}')`;
-}
-
-// Function to calculate the width based on image width
-function calculateWidth(imageUrl) {
-  const img = new Image();
-  img.src = imageUrl;
-  return img.width;
-}
-
-// Function to create and append flex items to the container
-function createFlexItems(container, count) {
-  for (let i = 0; i < count; i++) {
-    const flexItem = document.createElement('div');
-    flexItem.classList.add('flex-item');
-
-    const imageUrl = getRandomImageUrl();
-    setImageAsBackground(flexItem, imageUrl);
-
-    // Get the image width and set it as the flex item's width
-    const imageWidth = calculateWidth(imageUrl);
-    flexItem.style.width = `${imageWidth}px`;
-
-    container.appendChild(flexItem);
-
-    // Change the image every 3 seconds
-    setInterval(() => {
-      const newImageUrl = getRandomImageUrl();
-      setImageAsBackground(flexItem, newImageUrl);
-
-      // Update the width based on the new image width
-      const newImageWidth = calculateWidth(newImageUrl);
-      flexItem.style.width = `${newImageWidth}px`;
-    }, 3000);
-  }
-}
-
+// Get the container and create the flex items
 const imageContainer = document.getElementById('imageContainer');
 createFlexItems(imageContainer, 12); // Change 12 to the number of flex items you want
